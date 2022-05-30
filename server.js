5d50c1a2c6b80d39ee236330f9dbbd4a430c70e5
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-05-31 00:39:13 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+require('dotenv').config();
+const cookieParser = require("cookie-parser")
+
+const app = express();
+const PORT = process.env.PORT || 5040
+
+
+
+
+app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+
+const mongoUrl = process.env.MongoURL;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology : true })
+.then(result => {
+    if(result)
+    console.log("connected to todo-mongo: successful")
+}).catch(err => {
+    console.log(err)
+})
+
+
+
+app.listen(PORT, () => console.log(`server running on port ${PORT} `));
